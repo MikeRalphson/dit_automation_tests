ddd612b7c26c31059fe5d1d87a1fc204d658255e
john wakeling <john.wakeling@itv.com>
2012-08-24 10:49:11 +0100

    Added definitions for postcode_service

--- features/step_definitions/post_code_service.rb
+++ features/step_definitions/post_code_service.rb
@@ -7,12 +7,15 @@ Before do
   @response = ''
 end
 
-Given /^I request the postcode service with the following (.*)$/ do |postcode, broadcaster|
-  @uri = "#{EnvConfig['mercury_url']}#{uri}"
+Given /^I request the postcode service with the following (.+)$/ do |postcode|
+  @uri = URI.parse("#{EnvConfig['mercury_url']}/api/json/postcode/#{postcode.gsub(/\s+/,"")}")
   @response = @mercury_api.get_response_from_url @uri
 end
 
-Then /^I should get the correct <broadcaster> returned$/ do
-	pending #wd
+Then /^I should get the correct (\w+) returned$/ do |broadcaster|
+  json = @mercury_api.parse_json_response @response
+  unless @mercury_api.value_exists_in_json_hash? json, broadcaster, "RegionInfo", "Broadcaster"
+    raise "could not find the expected Boradcaster value: #{broadcaster} in the response for uri: #@uri"
+  end
 end
 
