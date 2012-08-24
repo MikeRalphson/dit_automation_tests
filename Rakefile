688fd1182bd7158c174cfd3800c8d680ea5d751b
Ben Snape <ben.snape@itv.com>
2012-08-24 15:17:36 +0100

    Removed environment profiles from cucumber.yml and implemented a more scalable solution in env.rb instead

--- Rakefile
+++ Rakefile
@@ -4,11 +4,12 @@ require 'set'
 require 'nokogiri'
 
 Cucumber::Rake::Task.new(:cucumber) do |task|
-  environment = ARGV.last
-  task.cucumber_opts = "-f pretty -p #{environment} -t ~@wip -t ~@not_implemented -t ~@manual"
+  ENV['environment'] ||= ARGV.last
+  task.cucumber_opts = "-f pretty -t ~@wip -t ~@not_implemented -t ~@manual"
 end
 
 Cucumber::Rake::Task.new(:wip) do |task|
+  ENV['environment'] ||= ARGV.last
   task.cucumber_opts = %w(-f pretty -t @wip)
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,23 @@
 require 'simplecov'
+require 'yaml'
+
 SimpleCov.start
+
+config = YAML::load(File.read(File.dirname(__FILE__) + '/../../config.yml'))
+
+case ENV['environment'] ||= 's01'
+  when 'rc2'
+    config.each { |k, v| v.each_pair { |x, y| ENV["#{x}"] = y } if k == 'rc2' }
+  when 'rc3'
+    config.each { |k, v| v.each_pair { |x, y| ENV["#{x}"] = y } if k == 'rc3' }
+  when 'rcx'
+    config.each { |k, v| v.each_pair { |x, y| ENV["#{x}"] = y } if k == 'rcx' }
+  when 's01'
+    config.each { |k, v| v.each_pair { |x, y| ENV["#{x}"] = y } if k == 's01' }
+  when 'i01'
+    config.each { |k, v| v.each_pair { |x, y| ENV["#{x}"] = y } if k == 'i01' }
+  when 'live'
+    config.each { |k, v| v.each_pair { |x, y| ENV["#{x}"] = y } if k == 'live' }
+  else
+    raise "error loading the requested environment: #{ENV["environment"]}"
+end
