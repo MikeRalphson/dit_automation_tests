604dda515ef635ff4117f9964e0038bace16de01
Ben Snape <ben.snape@itv.com>
2012-09-19 17:14:38 +0100

    fixed bug in both methods where the file wasn't returned and added a generic regex to file contents method

--- lib/xml_library.rb
+++ lib/xml_library.rb
@@ -5,6 +5,7 @@ class XmlLibrary
     nodes = xml.css(selector)
     nodes.each { |n| n.content = replace }
     File.open(path, "w") { |f| f.puts xml }
+    File.open(path) # must open again after writing
   end
 
   def xpath_replace_nodes(path, selector, replace)
@@ -12,6 +13,13 @@ class XmlLibrary
     nodes = xml.xpath(selector)
     nodes.each { |n| n.content = replace }
     File.open(path, "w") { |f| f.puts xml }
+    File.open(path) # must open again after writing
+  end
+
+  def apply_regex_to_file(path, regex, replace)
+    contents = File.open(path) { |f| f.read }
+    new_contents = contents.gsub(/#{regex}/, replace)
+    File.open(path, "w") { |f| f.puts new_contents }
   end
 
 end
