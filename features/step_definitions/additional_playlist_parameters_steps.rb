43040393e7f6c4b0fa47b0f561820528d90528f8
Ben Snape <ben.snape@itv.com>
2012-09-26 16:54:14 +0100

    added pending steps

---
+++ features/step_definitions/additional_playlist_parameters_steps.rb
@@ -0,0 +1,47 @@
+Given /^I request the Mercury playlist for (.*) and (.*) with null additional parameters$/ do |platform, vodcrid|
+  pending
+end
+
+Then /^the response should contain ad-server URL's with empty additional parameter values$/ do
+  pending
+end
+
+Given /^I request the Mercury playlist for (.*) and (.*) without additional parameters$/ do |platform, vodcrid|
+  pending
+end
+
+Then /^the response should contain ad-server URL's with no additional parameters$/ do
+  pending
+end
+
+Given /^I request the Mercury playlist for (.*) and (.*) with additional parameters$/ do |platform, vodcrid|
+  pending
+end
+
+Then /^the response should contain ad-server URL's with correctly appended additional parameters$/ do
+  pending
+end
+
+Given /^I request the Mercury playlist for (.*) and (.*) with additional parameters over 100 characters long$/ do |platform, vodcrid|
+  pending
+end
+
+Then /^the response should contain ad-server URL's with truncated additional parameters$/ do
+  pending
+end
+
+Given /^I request the Mercury playlist for (.*) and (.*) with additional parameters containing special characters$/ do |platform, vodcrid|
+  pending
+end
+
+Then /^the response should contain dots for special characters in the ad-server URL's$/ do
+  pending
+end
+
+Given /^I request the Mercury playlist for (.*) and (.*) with additional parameters in uppercase$/ do |platform, vodcrid|
+  pending
+end
+
+Then /^the response should contain lowercase characters in the ad-server URL's$/ do
+  pending
+end
