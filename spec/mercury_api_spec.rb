15fd250c4cb709cc248a37678465e3d129d77f9b
Ben Snape <ben.snape@itv.com>
2012-12-07 20:40:54 +0000

    added rspec tests for the library methods
    Change-Id: Ie8b5fa9d7f5e51a381181afcd438c63c9a372299

---
+++ spec/mercury_api_spec.rb
@@ -0,0 +1,41 @@
+# local libs
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'mercury_api'
+require 'http_client'
+
+describe "the Mercury API library" do
+
+  before(:each) do
+    @mercury = MercuryApi.new
+  end
+
+  describe "the recursive_find method"
+
+  it "should find a simple hash value" do
+    hash = {:first => 'ben', :last => 'snape'}
+    @mercury.recursive_find(:last, hash).should == 'snape'
+  end
+
+  it "should find a nested hash value" do
+    hash = {:person => {:title => 'mr', :name => {:first => 'ben', :last => 'snape'}, :interests => 'football'}}
+    @mercury.recursive_find(:first, hash).should == 'ben'
+  end
+
+  it "should find a value after a nested hash" do
+    hash = {:person => {:title => 'mr', :name => {:first => 'ben', :last => 'snape'}, :interests => 'football'}}
+    @mercury.recursive_find(:interests, hash).should == 'football'
+  end
+
+  describe "the get_response_from_url method"
+
+  it "should return a response from www.google.com" do
+    @mercury.get_response_from_url("http://www.google.com").should_not == nil?
+  end
+
+  describe "the calculate_week method"
+
+  it "should return seven different values" do
+    @mercury.calculate_week.to_set.size.should == 7
+  end
+
+end
