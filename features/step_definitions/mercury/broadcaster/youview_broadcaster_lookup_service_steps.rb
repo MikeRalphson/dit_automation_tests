e29a3cfa7a4e277479ee9ad3391919d9d1167856
Ben Snape <ben.snape@itv.com>
2013-06-16 22:29:43 +0100

    removed exceptions and properly using assertions
    Change-Id: I6db1c5bd33fce475f272f1d3546b35eb1810a27a

--- features/step_definitions/mercury/broadcaster/youview_broadcaster_lookup_service_steps.rb
+++ features/step_definitions/mercury/broadcaster/youview_broadcaster_lookup_service_steps.rb
@@ -3,15 +3,9 @@ When /^I make a YouView Broadcaster Lookup request for (.*)$/ do |region|
 end
 
 Then /^the response should contain (.*) broadcaster$/ do |broadcaster|
-  xml = @response.to_xml!
-  unless @mercury_api.value_exists_in_xml_node?(xml, 'Broadcaster', broadcaster)
-    raise "Incorrect Broadcaster region found was expecting #{broadcaster}"
-  end
+  @response.to_xml!.xpath('//Broadcaster').text.should == broadcaster
 end
 
 Then /^the response should contain (.*) error$/ do |error|
-  xml = @response.to_xml!
-  unless @mercury_api.value_exists_in_xml_node?(xml, 'Error', error)
-    raise "The following message is returned: #{error}"
-  end
+  @response.to_xml!.xpath('//Error').text.should == error
 end
