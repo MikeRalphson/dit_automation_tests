f9d9ba75d57821012ce000b03ad9a71b1d9275cc
Kiran Hare <kiran.hare@itv.com>
2013-12-02 11:40:36 +0000

    New Samsung HLS platform

--- lib/platform/samsunghls.rb
+++ lib/platform/samsunghls.rb
@@ -11,12 +11,12 @@ class Samsunghls < Platform
         :StreamType => 'HLS', # must be specified
     }
     @playlist_request.data[:request][:HLSRequestForMaster] = true
-    @playlist_response = Mercury::SamsungResponse.new
+    @playlist_response = Mercury::Response.new
   end
 
   def request_playlist
     @playlist_request.data[:deviceInfo] = @params
-    @playlist_request.data[:siteInfo][:Platform] = 'Samsunghls'
+    @playlist_request.data[:siteInfo][:Platform] = 'Samsung'
     super
   end
 
