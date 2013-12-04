a51b04705d2e1f4b380065c9e3d5210cfb458d01
Kiran Hare <kiran.hare@itv.com>
2013-12-04 11:20:51 +0000

    re-used the Samsung class instead of inheriting from the platform class

--- lib/platform/samsunghls.rb
+++ lib/platform/samsunghls.rb
@@ -1,22 +1,17 @@
-class Samsunghls < Platform
-
-  attr_reader :bitrates
+class Samsunghls < Samsung
 
   attr_accessor :params
 
   def initialize(category = 'catchup')
     super()
-    @bitrates = [120000]
     @params = {
         :StreamType => 'HLS', # must be specified
     }
     @playlist_request.data[:request][:HLSRequestForMaster] = true
-    @playlist_response = Mercury::Response.new
   end
 
   def request_playlist
     @playlist_request.data[:deviceInfo] = @params
-    @playlist_request.data[:siteInfo][:Platform] = 'Samsung'
     super
   end
 
