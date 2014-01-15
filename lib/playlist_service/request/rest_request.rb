1e1b0a09b08fb622f7cd94c5ffb4f631a4e4d1fb
Kiran Hare <kiran.hare@itv.com>
2014-01-15 09:48:54 +0000

    Created http_request method which only uses http

--- lib/playlist_service/request/rest_request.rb
+++ lib/playlist_service/request/rest_request.rb
@@ -12,6 +12,12 @@ module PlaylistService
                        :headers => {'Content-Type' => 'application/json'}
       end
 
+      def http_request(platform)
+        http_url = "#{EnvConfig['playlist_service']}/playlist/#@broadcaster/#{platform}/#@productionid".gsub('https', 'http')
+        HTTParty.get http_url,
+                     :headers => {'Content-Type' => 'application/json'}
+      end
+
     end
 
 end
