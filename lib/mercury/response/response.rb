079fa9dc1c42d653e61e60ba9e2b6d0ba3a02693
Kiran Hare <kiran.hare@itv.com>
2014-01-15 16:52:02 +0000

    Moved break_pattern function from MobileResponse to Response so all platforms can use it

--- lib/mercury/response/response.rb
+++ lib/mercury/response/response.rb
@@ -103,6 +103,14 @@ module Mercury
       @response.xpath('//ContentBreak')
     end
 
+    def break_pattern
+      pattern = []
+      (1..content_breaks.count).to_a.each do |n|
+        pattern << @response.xpath("//ContentBreak[#{n}]/Action").map { |advert| advert.attr('type') }
+      end
+      pattern.map! { |content_break| content_break.map { |ad_type| ad_type.split(/\A(\w)\w*-?(\w?)\w*$/).join.upcase } }
+    end
+
     def check_unauthorised_access
       @response.xpath('//Error').text
     end
