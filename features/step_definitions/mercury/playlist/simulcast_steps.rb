40211b076ad55f02e71037c08d0bfa57addf826c
Kiran Hare <kiran.hare@itv.com>
2014-02-04 10:00:32 +0000

    Updated steps to use simulcast helper

--- features/step_definitions/mercury/playlist/simulcast_steps.rb
+++ features/step_definitions/mercury/playlist/simulcast_steps.rb
@@ -1,9 +1,11 @@
-When(/^I request a simulcast playlist using (\w+\d+)$/) do |vodcrid|
+When(/^I request a simulcast playlist for (.*)$/) do |channel|
+  @channel = channel
+  @vodcrid = channel_to_simulcast_vodcrid(@channel)
   begin
-    @platform.playlist_request.data[:Vodcrid][:Id] = vodcrid
+    @platform.playlist_request.data[:Vodcrid][:Id] = @vodcrid
     @platform.request_simulcast_playlist
   rescue Savon::SOAP::Fault => error
-    @playlist_error = error
+    raise "There was a problem making the simulcast playlist request. The error was: #{error}"
   end
 end
 
@@ -12,19 +14,24 @@ Then(/^I should get a video type of simulcast$/) do
 end
 
 
-Then(/^I should get (\w+\d+) in the response$/) do |vodcrid|
-  @platform.playlist_response.simulcast_vodcrid.should include vodcrid
+Then(/^I should get the correct vodcrid back$/) do
+  @platform.playlist_response.simulcast_vodcrid.should include @vodcrid
 end
 
-Then(/^I should receive a valid playlist containing 3 (.*) with (.*)$/) do |stream_id, channel|
-  bit_rate = [1200, 600, 800]
+Then(/^I should receive a valid playlist containing 3 streams$/) do
   @platform.playlist_response.simulcast_stream_channels.size.should == 3
+
+  bit_rate = [1200, 600, 800]
   streams = @platform.playlist_response.simulcast_stream_channels.sort
+  stream_id = channel_to_stream_id(@channel)
+
   streams.each_with_index do |stream, index|
-    stream.should include "#{channel}livefms_#{bit_rate[index]}@#{stream_id}"
+    stream.should include "#{@channel}livefms_#{bit_rate[index]}@#{stream_id}"
   end
 end
 
-Then(/^I should get the (.*) in the response$/) do |base_url|
-  @platform.playlist_response.simulcast_base_url.should include base_url
+Then(/^I should get the correct base URL in the response$/) do
+  content_protection_code = channel_to_content_protection_code(@channel)
+  base_url = ["rtmpe://cp#{content_protection_code}.live.edgefcs.net/live"]
+  @platform.playlist_response.simulcast_base_url.should == base_url
 end
