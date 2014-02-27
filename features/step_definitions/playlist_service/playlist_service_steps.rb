6184795163eb61e97933b5e0ece1786bc11d25d5
Kiran Hare <kiran.hare@itv.com>
2014-02-27 10:59:49 +0000

    Invalid prod id steps for new playlist service

--- features/step_definitions/playlist_service/playlist_service_steps.rb
+++ features/step_definitions/playlist_service/playlist_service_steps.rb
@@ -1,3 +1,13 @@
+Given(/^I have a valid production id$/) do
+  @platform.productionid = "#{EnvConfig['playlist_production']}"
+end
+
+Given(/^I have an invalid production id$/) do
+  @invalid_prodid = "#{EnvConfig['playlist_production']}".gsub('-','_')
+  p @invalid_prodid
+  @platform.productionid = @invalid_prodid
+end
+
 When(/^I request the new playlist service$/) do
   @platform_to_s = @platform.class.to_s.downcase
   @platform.request_rest_playlist(@platform_to_s)
@@ -5,6 +15,7 @@ When(/^I request the new playlist service$/) do
 end
 
 When(/^I request the new playlist service via http$/) do
+  @platform.productionid = "#{EnvConfig['playlist_production']}"
   @platform_to_s = @platform.class.to_s
   @response = @platform.playlist_rest_request.http_request(@platform_to_s)
 end
@@ -29,4 +40,12 @@ end
 
 Then(/^I should get csmil in the media files url$/) do
   @platform.playlist_rest_response.media_file.should include 'csmil'
+end
+
+Then(/^I should get a valid error response$/) do
+  @platform.playlist_rest_response.error_message.should include "The ProductionId #{@invalid_prodid} Is Not In The Correct Format"
+end
+
+Then(/^the status code should be 400$/) do
+  @platform.playlist_rest_response.response_code.should == 400
 end
