2cd500ef9917bf19baac6f1a38a65a4b64cb3aef
Kiran Hare <kiran.hare@itv.com>
2014-03-06 14:04:18 +0000

    token needs to be in uppercase otherwise the request gives 403

--- lib/playlist_service/hmac_tokens.rb
+++ lib/playlist_service/hmac_tokens.rb
@@ -19,7 +19,8 @@ module PlaylistService
     def generate_hmac_token(productionid, platform)
       prodid = productionid.gsub("-","/").gsub("_", "#")
       saltedhash = @shared_key[:"#{platform}"] + prodid
-      Digest::SHA1.hexdigest saltedhash
+      hmac = Digest::SHA1.hexdigest saltedhash
+      hmac.upcase
     end
 
   end
