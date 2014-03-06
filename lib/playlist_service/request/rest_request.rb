3a93b920865bfd833e4bbd87be01b53cc12dc0b8
Kiran Hare <kiran.hare@itv.com>
2014-03-06 14:05:58 +0000

    Removed logging

--- lib/playlist_service/request/rest_request.rb
+++ lib/playlist_service/request/rest_request.rb
@@ -17,18 +17,14 @@ module PlaylistService
       end
 
       def do(platform, token)
-        p token
-        p @productionid
-        p "#{@uri}/playlist/#{@broadcast}/#{platform}/#{@productionid}"
-
         HTTParty.get "#{@uri}/playlist/#{@broadcast}/#{platform}/#{@productionid}",
-                       :headers => {'Content-Type' => 'application/json', 'hmac' => "#{token}"}
+                       :headers => {'Content-Type' => 'application/json', 'hmac' => token}
       end
 
-      def http_request(platform)
+      def http_request(platform, token)
         http_url = "#{@uri}/playlist/#{@broadcast}/#{platform}/#{@productionid}".gsub('https', 'http')
         HTTParty.get http_url,
-                     :headers => {'Content-Type' => 'application/json'}
+                     :headers => {'Content-Type' => 'application/json', 'hmac' => token}
       end
 
     end
