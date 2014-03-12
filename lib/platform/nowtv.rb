1f494c6ed244c5e2a330f0988e667323c4eb5c86
Kiran Hare <kiran.hare@itv.com>
2014-03-12 10:19:19 +0000

    created new NowTV platform class

---
+++ lib/platform/nowtv.rb
@@ -0,0 +1,20 @@
+class Nowtv < Platform
+
+  attr_accessor :params, :bitrates
+
+  def initialize(category = 'catchup')
+    super()
+    @params = {
+        :StreamType => 'HLS', # must be specified
+    }
+    @playlist_request.data[:request][:HLSRequestForMaster] = true
+    @bitrates = [1200000]
+  end
+
+  def request_playlist
+    @playlist_request.data[:siteInfo][:Platform] = 'SamsungTV'
+    @playlist_request.data[:deviceInfo] = @params
+    super
+  end
+
+end
