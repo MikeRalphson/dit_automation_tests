d1e1e9f25a2da96607362145edd12d7cfd427035
Kiran Hare <kiran.hare@itv.com>
2014-03-24 10:55:38 +0000

    Refactored playlist service features

--- features/step_definitions/playlist_service/playlist_service_steps.rb
+++ features/step_definitions/playlist_service/playlist_service_steps.rb
@@ -3,12 +3,12 @@ Given(/^I have a broadcast type of (\w+)$/) do |broadcast|
   @platform.playlist_rest_request.broadcast = @broadcast
 end
 
-Given(/^I have an invalid production id$/) do
+Given(/^I have an invalid format production id$/) do
   @invalid_prodid = "#{EnvConfig['playlist_production']}".gsub('-','_')
   @platform.playlist_rest_request.productionid = @invalid_prodid
 end
 
-Given(/^I have no asset information$/) do
+Given(/^I have no asset information for that production id$/) do
   @platform.playlist_rest_request.productionid = "#{EnvConfig['no_assets_prodid']}"
 end
 
@@ -36,11 +36,11 @@ When(/^I request the playlist service with a blank hmac token for supported plat
     @response = @platform.playlist_rest_request.blank_hmac_token(@platform.class.to_s.downcase)
 end
 
-Then(/^I should get a valid status code$/) do
+Then(/^I should get a 200 status code$/) do
   @platform.class.to_s == 'Samsung' || @platform.class.to_s == 'Android' ? @platform.playlist_rest_response.response_code.should == 200 : @platform.playlist_rest_response.response_code.should == 501
 end
 
-Then(/^I get the correct production ID$/) do
+Then(/^I should get the correct production ID$/) do
   if @platform_to_s == 'Samsung' || @platform_to_s == 'Android'
     @platform.playlist_rest_response.production_id.should match "#{EnvConfig['playlist_production']}"
   end
@@ -54,33 +54,33 @@ Then(/^I should get csmil in the media files url$/) do
   @platform.playlist_rest_response.media_file.should include 'csmil'
 end
 
-Then(/^I should get a valid error response for invalid format$/) do
+Then(/^I should get an invalid format response message$/) do
   @platform.playlist_rest_response.rest_error_message.should include "The ProductionId #{@invalid_prodid} Is Not In The Correct Format"
 end
 
-Then(/^the status code should be 400$/) do
+Then(/^then I should get a 400 status code$/) do
   @platform.playlist_rest_response.response_code.should == 400
 end
 
-Then(/^I should get a valid platform not supported message$/) do
+Then(/^I should get a platform not supported message$/) do
   platform = @platform.playlist_rest_request.platform
   @platform.playlist_rest_response.rest_error_message.should include "The Playlist Functionality For The Platform #{platform} Is Not Available"
 end
 
-Then(/^I should get a valid response for broadcast type not implemented$/) do
+Then(/^I should get a broadcast not implemented response$/) do
   @platform.playlist_rest_response.rest_error_message.should include "The Playlist Functionality For The Broadcaster #{@broadcast} Is Not Available"
 end
 
-Then(/^I should get a valid response indicating no assets found$/) do
+Then(/^I should get a no assets found response$/) do
   @no_assets_prod_id = "#{EnvConfig['no_assets_prodid']}".gsub('-','/').gsub('_','#')
   @platform.playlist_rest_response.rest_error_message.should include "Asset Information For Production Id #{@no_assets_prod_id} Is Not Found"
 end
 
-Then(/^the status code should be 501$/) do
+Then(/^then I should get 501 status code$/) do
   @platform.playlist_rest_response.response_code.should == 501
 end
 
-Then(/^I should get a 404 no content status code$/) do
+Then(/^I should get a 404 not found status code$/) do
   @platform.playlist_rest_response.response_code.should == 404
 end
 
@@ -100,8 +100,8 @@ Then(/^the response should contain a base uri populated with the Akamai config$/
   @platform.playlist_rest_response.base_uri.should include "itvandroidhls-vh.akamaihd.net/i/"
 end
 
-Then(/^I should get a match uri And bitrate pattern error$/) do
-  #new playlist requires the format in the rendition names to be: itv_{bitrate}
+Then(/^I should get a match uri And bitrate pattern error response$/) do
+  #new playlist requires the format in the rendition names to be: itv{bitrate}
   #Example: @"(?<StartUri>[\W\w]+)(?<Partner>(?i)_itv)(?<Bitrate>[0-9]+)(?<EndUri>[\W\w]+)"
   #Example Android: @"(?<StartUri>[\W\w]+)(?<Partner>(?i)_bl)(?<Bitrate>[0-9]+)(?<EndUri>[\W\w]+)"
   @playlist_error.to_s.should include "Unable To Match Uri And Bitrate Pattern"
